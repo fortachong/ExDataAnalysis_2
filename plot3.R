3d170c95516de42ca589d169ea71fdd48ff0eed0
fortachong <darkforta@gmail.com>
2015-06-19 18:47:17 -0500

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,20 +2,18 @@
 library(ggplot2)
 # Download the file from the official URL
 # https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
-# download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", 
-#              method="auto", destfile = "data.zip")
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", 
+              method="auto", destfile = "data.zip")
 
 
 # Read Files in R format
-#NEI <- readRDS(unzip("data.zip", "summarySCC_PM25.rds"))
-#SCC <- readRDS(unzip("data.zip", "Source_Classification_Code.rds"))
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+NEI <- readRDS(unzip("data.zip", "summarySCC_PM25.rds"))
+SCC <- readRDS(unzip("data.zip", "Source_Classification_Code.rds"))
 
 # Filter data from Baltimore City (fips = 24510), select columns: Emissions, year, type
 Z <- NEI[NEI$fips=="24510", c("Emissions", "year", "type")]
 # Aggregate data by year and type (Sum of Emissions)
-Y <- aggregate(Z["Emissions"], by=c(Z["year"],Z["type"]), FUN=sum)
+Y <- aggregate(Z["Emissions"], data=Z, by=c(Z["year"],Z["type"]), FUN=sum)
 # Open file
 png(file="plot3.png", width=480, height=480)
 # plot using year in the x axis, Emissions in the y axis, group by type
